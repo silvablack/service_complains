3561840559c4e5d50feefa09bcf06a2444deb55b
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-20 19:10:05 -0300

    test integration to POST /complains

--- tests/integration/app.js
+++ tests/integration/app.js
@@ -11,8 +11,8 @@ describe('Test Integration', () => {
       uf: 'MA',
     },
     company_id: '5b74e44d6906800036631800',
-    date_created: Date.now(),
-    date_updated: Date.now(),
+    date_created: '2018-08-20T21:33:22.394Z',
+    date_updated: '2018-08-20T21:33:22.394Z',
   };
 
   beforeEach((done) => {
@@ -48,6 +48,37 @@ describe('Test Integration', () => {
           expect(res.body.description).to.be.eql(defaultComplains.description);
           expect(res.body.locale.city).to.be.eql(defaultComplains.locale.city);
           expect(res.status).to.be.eql(200);
+          done(err);
+        });
+    });
+  });
+
+  describe('Route /POST /complains/', () => {
+    const newComplain = {
+      _id: '5b74e44d6906800036631901',
+      title: 'Corte Ilegal',
+      description: 'Lorem ipsum massa lacus justo diam molestie vulputate, luctus dolor fermentum habitant dictum et, taciti dapibus consectetur cras metus curabitur. feugiat ut odio nec nostra enim adipiscing aenean lectus elit, risus rutrum torquent augue posuere mi eu turpis, nisl molestie porta nulla tincidunt consectetur leo varius. sem porta in nisi sociosqu eleifend habitant lectus suscipit ligula nunc, commodo tristique eget dictum iaculis varius dictumst adipiscing semper. imperdiet erat orci inceptos quam sed et platea, gravida eleifend malesuada aenean aliquet molestie vivamus, sem suspendisse suscipit est porttitor fringilla. malesuada hac lacus donec consectetur, mollis congue dolor varius nostra, fringilla sollicitudin velit.',
+      locale: {
+        city: 'São Luís',
+        state: 'Maranhão',
+        uf: 'MA',
+      },
+      company_id: '5b74e44d6906800036631800',
+      date_created: '2018-08-20T21:33:22.394Z',
+      date_updated: '2018-08-20T21:33:22.394Z',
+    };
+    it('should create a complain', (done) => {
+      request
+        .post('/complains')
+        .send(newComplain)
+        .end((err, res) => {
+          expect(res.body._id).to.be.eql(newComplain._id);
+          expect(res.body.title).to.be.eql(newComplain.title);
+          expect(res.body.description).to.be.eql(newComplain.description);
+          expect(res.body.locale.city).to.be.eql(newComplain.locale.city);
+          expect(res.body.date_created).to.be.eql(newComplain.date_created);
+          expect(res.body.company_id).to.be.eql(newComplain.company_id);
+          expect(res.status).to.be.eql(201);
           done(err);
         });
     });
