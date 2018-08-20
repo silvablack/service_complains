f8f837c03aa5899a0e23623f9add5f0d71ae3876
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-20 20:15:46 -0300

    add test complains update

--- tests/unit/controllers/complains.js
+++ tests/unit/controllers/complains.js
@@ -92,4 +92,40 @@ describe('Controller Complains', () => {
       });
     });
   });
+  describe('Update Complain', () => {
+    it('should update a complain', (done) => {
+      const Complains = {
+        update: td.function(),
+      };
+      const updateComplain = [{
+        title: 'Corte Ilegal',
+        description: 'Luctus dolor fermentum habitant dictum et, taciti dapibus consectetur cras metus curabitur. feugiat ut odio nec nostra enim adipiscing aenean lectus elit, risus rutrum torquent augue posuere mi eu turpis, nisl molestie porta nulla tincidunt consectetur leo varius. sem porta in nisi sociosqu eleifend habitant lectus suscipit ligula nunc, commodo tristique eget dictum iaculis varius dictumst adipiscing semper. imperdiet erat orci inceptos quam sed et platea, gravida eleifend malesuada aenean aliquet molestie vivamus, sem suspendisse suscipit est porttitor fringilla. malesuada hac lacus donec consectetur, mollis congue dolor varius nostra, fringilla sollicitudin velit.',
+        locale: {
+          city: 'São Luís',
+          state: 'Maranhão',
+          uf: 'MA',
+        },
+        date_create: Date.now(),
+      }];
+      const expectResponse = [{
+        _id: '5b74e44d6906800036631900',
+        title: 'Corte Ilegal',
+        description: 'Lorem ipsum massa lacus justo diam molestie vulputate, luctus dolor fermentum habitant dictum et, taciti dapibus consectetur cras metus curabitur. feugiat ut odio nec nostra enim adipiscing aenean lectus elit, risus rutrum torquent augue posuere mi eu turpis, nisl molestie porta nulla tincidunt consectetur leo varius. sem porta in nisi sociosqu eleifend habitant lectus suscipit ligula nunc, commodo tristique eget dictum iaculis varius dictumst adipiscing semper. imperdiet erat orci inceptos quam sed et platea, gravida eleifend malesuada aenean aliquet molestie vivamus, sem suspendisse suscipit est porttitor fringilla. malesuada hac lacus donec consectetur, mollis congue dolor varius nostra, fringilla sollicitudin velit.',
+        locale: {
+          city: 'São Luís',
+          state: 'Maranhão',
+          uf: 'MA',
+        },
+        company_id: '5b74e44d6906800036631800',
+        date_create: Date.now(),
+        date_updated: Date.now(),
+      }];
+      td.when(Complains.update(updateComplain.id, updateComplain)).thenResolve(expectResponse);
+      const complainsController = new ComplainsController(Complains);
+      complainsController.put(updateComplain.id, updateComplain).then((response) => {
+        expect(response.data).to.be.eql(expectResponse);
+        done();
+      });
+    });
+  });
 });
