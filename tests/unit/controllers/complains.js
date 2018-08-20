f90cfc0b6ac2954a5bd99f6b5e003ecdd61313ec
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-20 19:09:29 -0300

    test unit controller method create

--- tests/unit/controllers/complains.js
+++ tests/unit/controllers/complains.js
@@ -53,4 +53,43 @@ describe('Controller Complains', () => {
       });
     });
   });
+  describe('Create Complain', () => {
+    it('should create a company', (done) => {
+      const Complains = {
+        create: td.function(),
+      };
+      const newCompany = [{
+        _id: '5b74e44d6906800036631900',
+        title: 'Corte Ilegal',
+        description: 'Lorem ipsum massa lacus justo diam molestie vulputate, luctus dolor fermentum habitant dictum et, taciti dapibus consectetur cras metus curabitur. feugiat ut odio nec nostra enim adipiscing aenean lectus elit, risus rutrum torquent augue posuere mi eu turpis, nisl molestie porta nulla tincidunt consectetur leo varius. sem porta in nisi sociosqu eleifend habitant lectus suscipit ligula nunc, commodo tristique eget dictum iaculis varius dictumst adipiscing semper. imperdiet erat orci inceptos quam sed et platea, gravida eleifend malesuada aenean aliquet molestie vivamus, sem suspendisse suscipit est porttitor fringilla. malesuada hac lacus donec consectetur, mollis congue dolor varius nostra, fringilla sollicitudin velit.',
+        locale: {
+          city: 'São Luís',
+          state: 'Maranhão',
+          uf: 'MA',
+        },
+        company_id: '5b74e44d6906800036631800',
+        date_create: Date.now(),
+        date_updated: Date.now(),
+      }];
+      const expectResponse = [{
+        _id: '5b74e44d6906800036631900',
+        title: 'Corte Ilegal',
+        description: 'Lorem ipsum massa lacus justo diam molestie vulputate, luctus dolor fermentum habitant dictum et, taciti dapibus consectetur cras metus curabitur. feugiat ut odio nec nostra enim adipiscing aenean lectus elit, risus rutrum torquent augue posuere mi eu turpis, nisl molestie porta nulla tincidunt consectetur leo varius. sem porta in nisi sociosqu eleifend habitant lectus suscipit ligula nunc, commodo tristique eget dictum iaculis varius dictumst adipiscing semper. imperdiet erat orci inceptos quam sed et platea, gravida eleifend malesuada aenean aliquet molestie vivamus, sem suspendisse suscipit est porttitor fringilla. malesuada hac lacus donec consectetur, mollis congue dolor varius nostra, fringilla sollicitudin velit.',
+        locale: {
+          city: 'São Luís',
+          state: 'Maranhão',
+          uf: 'MA',
+        },
+        company_id: '5b74e44d6906800036631800',
+        date_create: Date.now(),
+        date_updated: Date.now(),
+      }];
+      td.when(Complains.create(newCompany)).thenResolve(expectResponse);
+      const complainsController = new ComplainsController(Complains, false);
+      complainsController.post(newCompany).then((response) => {
+        expect(response.data).to.be.eql(expectResponse);
+        done();
+      });
+    });
+  });
 });
