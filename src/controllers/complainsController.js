61483189e329518148f0380ca47ea23d8d0605bf
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-20 00:46:14 -0300

    added controller method get all and get all from cache

---
+++ src/controllers/complainsController.js
@@ -0,0 +1,77 @@
+/**
+ * @author Paulo Silva
+ * @description Controller to Complains Service
+ * @returns methods <get, getById, post, put, delete>
+ * @version 1.0.0
+ */
+'use strict';
+
+/**
+ * @requires Http Status
+ */
+const HttpStatus = require('http-status');
+/**
+* @requires Redis Data Cache
+*/
+const redis = require('redis');
+
+/**
+* @description Interfaces to data response
+*/
+const defaultResponse = (data, statusCode = HttpStatus.OK) => ({
+  data,
+  statusCode,
+});
+
+const errorResponse = (message, statusCode = HttpStatus.BAD_REQUEST) => defaultResponse({
+  error: message,
+}, statusCode);
+
+class ComplainsController{
+
+    constructor(Complains){
+        this.ComplainsModel = Complains;
+    }
+    
+    /**
+     * @name callback for Request VERBS GET ALL
+     * @param {Request} req 
+     * @param {Response} res 
+     * @returns Promise ComplainsModel.getAll()
+     */
+    getAll(){
+        return new Promise((resolve,reject)=>{
+            this.ComplainsModel.getAll()
+            .then((complains)=>{
+                resolve(defaultResponse(complains));
+            }).catch(err => reject(errorResponse(err.message)));
+        });
+    }
+
+    /**
+     * @name getAllFromCache
+     * @description Return a response with all companies from cache
+     * @return Promise ComplainsModel.gellAllFromCache()
+     */
+    getAllFromCache() {
+        return new Promise((resolve, reject) => {
+        /**
+        * @constructor Redis Client
+        */
+        let client = redis.createClient(`redis://${process.env.URL_CACHE_COMPANY}`);
+        client.get('allComplains', (err, reply) => {
+            if (reply) {
+                resolve(defaultResponse(reply));
+            }
+        });
+        this.ComplainsModel.getAll()
+            .then((complains) => {
+                client.set('allComplains', JSON.stringify(complains));
+                client.expire('allComplains', 20);
+                resolve(defaultResponse(complains));
+            }).catch(erro => reject(errorResponse(erro.message)));
+        });
+    }
+}
+
+module.exports = ComplainsController;
