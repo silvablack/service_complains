cd520106fb517f4c25e68be34fee2a1c377b86e9
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-19 22:48:15 -0300

    add test integration and init server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/**
+ * @author Paulo Silva
+ * @description Server
+ * @returns app with instance of express 
+ * @version 1.0.0
+ */
+
+/**
+ * @requires express server
+ */
+const express = require('express');
+
+/**
+ * @constructor express()
+ */
+const app = express();
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * @author Paulo Silva
+ * @description Complains Server
+ * @returns listen(PORT,HOST)
+ * @version 1.0.0
+ */
+
+const app = require('./app');
+
+/**
+ * @description Define host and port to server listening
+ */
+const PORT = process.env.PORT_SERVICE_COMPANY || 5000;
+const HOST = process.env.URL_SERVICE_COMPANY || '0.0.0.0';
+
+/**
+ * @description init listening
+ */
+app.listen(PORT, HOST);
+console.log(`Servidor rodando em http://${HOST}:${PORT}`);
+
