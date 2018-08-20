001ec4ec1625b6479eb0d0b1dded82b3c70536c0
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-19 23:06:28 -0300

    connect to mongodb with define schema

---
+++ src/config/db.js
@@ -0,0 +1,57 @@
+'use strict';
+/**
+ * @author Paulo Silva
+ * @description Instance MongoDB with Mongoose, Build and Validate Schema
+ * @version 1.0.0
+ */
+
+ /**
+  * @requires mongoose
+  */
+const mongoose = require('mongoose');
+
+/**
+ * @description connect mongo db
+ */
+mongoose.connect(`mongodb://${process.env.URL_DATA_COMPLAINS}:${process.env.PORT_DATA_COMPLAINS}/db_challenge`,{ useNewUrlParser: true });
+
+/**
+ * @description Define Complains Doc Schema and Validator
+ */
+var complainsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    locale: {
+        city: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        uf: {
+            type: String,
+            required: true
+        }
+    },
+    company_id: {
+        type: String,
+        required: true
+    },
+    date_created: {type: Date, default: Date.now()},
+    date_updated: {type: Date}
+},{versionKey: false});
+
+/**
+ * Set Model Complains on Complains Schema
+ */
+var Complains = mongoose.model('Complains',complainsSchema);
+
+module.exports = Complains;
